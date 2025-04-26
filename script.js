7bec01004ace7b0c66f319278dd7aa244df9d9ca
koraykip <62715254+koraykip@users.noreply.github.com>
2025-04-26 09:07:11 +0200

    feat: add operate() function

--- script.js
+++ script.js
@@ -8,3 +8,17 @@ function divide(a, b)   {
   if (b === 0) throw new Error("Can't divide by zero!");
   return a / b;
 }
+
+
+function operate(operator, a, b) {
+    switch (operator) {
+      case "+": return add(a, b);
+      case "-": return subtract(a, b);
+      case "×": return multiply(a, b);
+      case "÷": return divide(a, b);
+      default:
+        throw new Error(`Unknown operator: ${operator}`);
+    }
+  }
+
+console.log( operate("+", 7, 3) ); // 10
